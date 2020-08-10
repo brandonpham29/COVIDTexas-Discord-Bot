c53ac2a741d6c72b7e132f10c2b1b01e9d25dfcc
brandonpham29 <brandonpham29@gmail.com>
2020-08-10 15:21:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,4 +39,4 @@ bot.on('message', async msg=> {
 
 })
 
-bot.login(process.env.NzQyNDUyMzg3MTU1MDE3Nzcx.XzGUqw.ZQ37YTRM_pJpwl5cnNwRkZTkLAA);
+bot.login(process.env.NzQyNDUyMzg3MTU1MDE3Nzcx.XzGUqw.0SGPUQ05nmsJjf2_TYe2eRg0ja0);
